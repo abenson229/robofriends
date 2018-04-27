b169bc1369601164709b4aff4fe94adc88c9613c
Adrian Benson <adrianbenson@Adrians-MacBook-Pro.local>
2018-04-26 23:33:33 -0400

    added tachyons, playing with component construction

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from "./Hello";
+import "tachyons";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={"Hello React Ninja"}/>, document.getElementById('root'));
 registerServiceWorker();
