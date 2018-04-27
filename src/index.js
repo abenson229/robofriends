af5b0f0c0293798327687eb716489df6bbdf5d60
ADRIAN-ALIENWAR\abens <adrian.benson@snhu.edu>
2018-04-27 15:39:35 -0400

    Added App and SearchBox components. Added app state.Connected searchbox to modify app state and the robots displayed.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Card from "./Card";
-import CardList from "./CardList";
-import {robots} from "./robots";
+// import CardList from "./CardList";
+import App from "./App";
+// import {robots} from "./robots";
 import "tachyons";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render (<CardList robots={robots}/>, document.getElementById('root'));
+ReactDOM.render (<App />, document.getElementById('root'));
 
 registerServiceWorker();
